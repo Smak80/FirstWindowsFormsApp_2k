a33befe81e482870b0e8176488e29658227e5a7a
Smak80 <smaklets@kpfu.ru>
2020-12-09 15:32:06 +0300

    Добавьте файлы проекта.

---
+++ FirstWindowsFormsApp/Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿
+namespace FirstWindowsFormsApp
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnPress = new System.Windows.Forms.Button();
+            this.userText = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnPress
+            // 
+            this.btnPress.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.btnPress.Location = new System.Drawing.Point(225, 61);
+            this.btnPress.Name = "btnPress";
+            this.btnPress.Size = new System.Drawing.Size(116, 38);
+            this.btnPress.TabIndex = 0;
+            this.btnPress.Text = "Нажми меня";
+            this.btnPress.UseVisualStyleBackColor = true;
+            this.btnPress.Click += new System.EventHandler(this.btnPress_Click);
+            // 
+            // userText
+            // 
+            this.userText.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.userText.Location = new System.Drawing.Point(12, 33);
+            this.userText.Name = "userText";
+            this.userText.Size = new System.Drawing.Size(540, 22);
+            this.userText.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(206, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Введите произвольный текст:";
+            // 
+            // lblResult
+            // 
+            this.lblResult.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.lblResult.AutoSize = true;
+            this.lblResult.Location = new System.Drawing.Point(13, 106);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(0, 17);
+            this.lblResult.TabIndex = 3;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(564, 132);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.userText);
+            this.Controls.Add(this.btnPress);
+            this.MaximumSize = new System.Drawing.Size(1024, 179);
+            this.MinimumSize = new System.Drawing.Size(400, 179);
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Первое приложение с графическим интерфейсом";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnPress;
+        private System.Windows.Forms.TextBox userText;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblResult;
+    }
+}
+
